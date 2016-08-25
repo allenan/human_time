f04ea8f3ed5bc38b3158f52f2cd2ddb49e145b6d
zp <block24block@gmail.com>
2016-08-25 21:39:18 +0800

    include HumanTime (#1)
    * handle mapping in module HumanTime, not in Concrete classes
    
    * reduce duplicated alias_method
    
    * code style: args in parens

--- lib/human_time.rb
+++ lib/human_time.rb
@@ -4,33 +4,43 @@ require 'time'
 require "human_time/version"
 
 module HumanTime
-  def self.greater_than_aliases
-    %w{newer_than? more_recent_than? comes_after? later_than?}
+  class << self
+    def greater_than_aliases
+      %w{newer_than? more_recent_than? comes_after? later_than?}
+    end
+
+    def greater_than_or_equal_to_aliases
+      %w{newer_than_or_equal_to? more_recent_than_or_equal_to? after_or_equal_to? later_than_or_equal_to?}
+    end
+
+    def less_than_aliases
+      %w{older_than? comes_before? earlier_than?}
+    end
+
+    def less_than_or_equal_to_aliases
+      %w{older_than_or_equal_to? before_or_equal_to? earlier_than_or_equal_to?}
+    end
+
+    def included(base)
+      add_alias(base, greater_than_aliases, :>)
+      add_alias(base, greater_than_or_equal_to_aliases, :>=)
+      add_alias(base, less_than_aliases, :<)
+      add_alias(base, less_than_or_equal_to_aliases, :<=)
+    end
+
+    private
+
+    def add_alias(base, aliases, op)
+      aliases.each{ |ali| base.send :alias_method, ali, op }
+    end
   end
 
-  def self.greater_than_or_equal_to_aliases
-    %w{newer_than_or_equal_to? more_recent_than_or_equal_to? after_or_equal_to? later_than_or_equal_to?}
-  end
-
-  def self.less_than_aliases
-    %w{older_than? comes_before? earlier_than?}
-  end
-
-  def self.less_than_or_equal_to_aliases
-    %w{older_than_or_equal_to? before_or_equal_to? earlier_than_or_equal_to?}
-  end
 end
 
 class Date
-  HumanTime.greater_than_aliases.each{ |ali| alias_method ali, :> }
-  HumanTime.greater_than_or_equal_to_aliases.each{ |ali| alias_method ali, :>= }
-  HumanTime.less_than_aliases.each{ |ali| alias_method ali, :< }
-  HumanTime.less_than_or_equal_to_aliases.each{ |ali| alias_method ali, :<= }
+  include HumanTime
 end
 
 class Time
-  HumanTime.greater_than_aliases.each{ |ali| alias_method ali, :> }
-  HumanTime.greater_than_or_equal_to_aliases.each{ |ali| alias_method ali, :>= }
-  HumanTime.less_than_aliases.each{ |ali| alias_method ali, :< }
-  HumanTime.less_than_or_equal_to_aliases.each{ |ali| alias_method ali, :<= }
+  include HumanTime
 end
